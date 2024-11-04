5bff1a58e749b942778aa2dba98cd1708974877b
Arad Alvand <aradalvand@gmail.com>
2024-11-04 23:10:26 +0330

    .

--- Program.cs
+++ Program.cs
@@ -1,33 +1,78 @@
-﻿using System.Diagnostics;
+﻿using System.Threading.Tasks.Dataflow;
+using System.Diagnostics;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
 
-Console.WriteLine("Here");
-var r = Subscribe();
-Console.WriteLine("Here now");
+Console.WriteLine(TaskScheduler.Current.MaximumConcurrencyLevel);
+// BenchmarkRunner.Run<ThroughputBenchmarks>();
 
-Stopwatch sw = new();
-sw.Start();
-List<Task> tasks = [];
-await foreach (var req in r)
-    tasks.Add(Handle(req));
-await Task.WhenAll(tasks);
-sw.Stop();
-Console.WriteLine($"Finished in {sw.Elapsed.TotalMilliseconds:N0} ms");
-
-Console.ReadKey();
-
-async Task Handle(int req)
+[MemoryDiagnoser(false)]
+[ShortRunJob]
+public class ThroughputBenchmarks
 {
-    await Task.Delay(1000);
-    // Console.WriteLine($"{req} handled.");
-}
-
-async IAsyncEnumerable<int> Subscribe()
-{
-    Console.WriteLine("Subscribe");
-    var random = new Random();
-    for (int i = 0; i < 1_000_000; i++)
+    [Benchmark]
+    public async Task TaskSpawning()
     {
-        // await Task.Delay(100);
-        yield return i;
+        List<Task> tasks = [];
+
+        await foreach (var req in Subscribe())
+            tasks.Add(Handle(req));
+
+        await Task.WhenAll(tasks);
+    }
+
+    [Benchmark]
+    public async Task WithSemaphore()
+    {
+        List<Task> tasks = [];
+
+        var semaphore = new SemaphoreSlim(int.MaxValue);
+        await foreach (var req in Subscribe())
+        {
+            await semaphore.WaitAsync();
+            tasks.Add(Handle(req).ContinueWith(_ => semaphore.Release()));
+        }
+
+        await Task.WhenAll(tasks);
+    }
+
+    [Benchmark]
+    public async Task ActionBlock()
+    {
+        var workerBlock = new ActionBlock<int>(
+            Handle,
+            new ExecutionDataflowBlockOptions
+            {
+                MaxDegreeOfParallelism = int.MaxValue
+            }
+        );
+        await foreach (var req in Subscribe())
+            workerBlock.Post(req);
+        workerBlock.Complete();
+        await workerBlock.Completion;
+    }
+
+
+    [Benchmark]
+    public async Task ParallelForEach()
+    {
+        await Parallel.ForEachAsync(Subscribe(), parallelOptions: new()
+        {
+            MaxDegreeOfParallelism = int.MaxValue
+        }, async (req, ct) =>
+        {
+            await Handle(req);
+        });
+    }
+
+    private static async Task Handle(int req)
+    {
+        await Task.Delay(1000);
+    }
+
+    private static async IAsyncEnumerable<int> Subscribe()
+    {
+        for (int i = 0; i < 1_000_000; i++)
+            yield return i;
     }
 }
