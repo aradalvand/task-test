df753bc0fe9f456ef7dafada0c76b62eadda6a59
Arad Alvand <aradalvand@gmail.com>
2024-11-04 16:30:11 +0330

    Initial

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿_ = Task.Run(async () =>
+{
+    while (true)
+    {
+        await Task.Delay(1000);
+        Console.WriteLine("1 second passed in task");
+    }
+});
+
+
+while (true)
+{
+    await Task.Delay(1000);
+    Console.WriteLine("1 second passed here");
+}
+
+Console.WriteLine("Yep");
+Console.ReadLine();
